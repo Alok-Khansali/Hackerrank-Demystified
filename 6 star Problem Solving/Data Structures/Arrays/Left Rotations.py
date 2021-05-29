2dd9f9b6ea4864221d9c56312e29c9576fb144c3
Alok Khansali <75980074+TheCodeAlpha26@users.noreply.github.com>
2021-05-29 16:40:58 +0530

    Create Left Rotations.py

---
+++ 6 star Problem Solving/Data Structures/Arrays/Left Rotations.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+def rotateLeft(d, arr):                     #YES THESE
+    return(arr[d:]+arr[:d])                 #TWO LINES
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    first_multiple_input = input().rstrip().split()
+    n = int(first_multiple_input[0])
+    d = int(first_multiple_input[1])
+    arr = list(map(int, input().rstrip().split()))
+    result = rotateLeft(d, arr)
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+    fptr.close()
