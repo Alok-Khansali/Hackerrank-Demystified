1671d5f3900b2a0d751499ad35e5b8e1e40da743
Alok Khansali <75980074+TheCodeAlpha26@users.noreply.github.com>
2021-06-04 21:50:30 +0530

    Update Java 1D Array.java

--- 5 Star Java/Java 1D Array.java
+++ 5 Star Java/Java 1D Array.java
@@ -1,6 +1,8 @@
 import java.util.*;
-public class Solution {
-    public static void main(String[] args) {
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int a[]=new int[n];
