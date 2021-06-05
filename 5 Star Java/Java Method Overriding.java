1f1c49f96e55db2a10fecd7432ec8a28dbf7ca1b
Alok Khansali <75980074+TheCodeAlpha26@users.noreply.github.com>
2021-06-05 12:52:29 +0530

    Create Java Method Overriding.java

---
+++ 5 Star Java/Java Method Overriding.java
@@ -0,0 +1,6 @@
+@Override                      // THIS THE CODE YOU HAVE TO WRITE IN THE EDITOR   
+                               //@Override is not necessary but it increases the readability of the code
+void getNumberOfTeamMembers()
+{
+        System.out.println( "Each team has 11 players in " + getName() );
+ }
