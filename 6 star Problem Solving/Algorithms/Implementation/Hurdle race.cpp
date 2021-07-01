477611d4a95e0fa97d36ea73521a79f57080803f
Alok Khansali <75980074+TheCodeAlpha26@users.noreply.github.com>
2021-07-01 22:31:08 +0530

    Update and rename Hurdle race to Hurdle race.cpp

--- 6 star Problem Solving/Algorithms/Implementation/Hurdle race.cpp
+++ 6 star Problem Solving/Algorithms/Implementation/Hurdle race.cpp
@@ -1,15 +1,9 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 vector<string> split_string(string);
 int hurdleRace(int k, vector<int> v) {
     int x=*max_element(v.begin(),v.end());
-    if(k>=x)
-      return(0);
-    else 
-       return(x-k);    
-    return(0);
+    return(k>=x?0:x-k);    
 }
 int main(){
     ios_base::sync_with_stdio(false);
@@ -19,7 +13,6 @@ int main(){
     vector<int> height(n);
     for (int i = 0; i < n; i++) 
         cin>>height[i];
-    int result = hurdleRace(k, height);
-    cout << result << "\n";
+    cout << hurdleRace(k, height) << "\n";
     return 0;
 }
