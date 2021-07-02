97bf3fa9044b576c4441b5d77ddd7cacc9a7dfc0
Alok Khansali <75980074+TheCodeAlpha26@users.noreply.github.com>
2021-07-02 18:34:11 +0530

    Update and rename Maximum Draws to Maximum Draws.java

---
+++ Mathematics/Fundamentals/Maximum Draws.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution {
+    public static void main(String args[])
+    {
+        Scanner I=new Scanner(System.in);
+        int t=I.nextInt();
+        while(t-->0)
+            System.out.println(I.nextInt()+1);
+    }
+}
