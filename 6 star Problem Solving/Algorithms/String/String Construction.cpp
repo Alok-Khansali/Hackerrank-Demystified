c1ea61e12669497e0b7cd7e4d6360c106a53c61b
Alok Khansali <75980074+TheCodeAlpha26@users.noreply.github.com>
2021-07-19 20:07:49 +0530

    Update and rename String Construction to String Construction.cpp

--- 6 star Problem Solving/Algorithms/String/String Construction.cpp
+++ 6 star Problem Solving/Algorithms/String/String Construction.cpp
@@ -6,7 +6,7 @@ int stringConstruction(string s)
        vector<int> v(26,0);
        for(int i=0;i<s.size();i++)
           v[s[i]-'a']++;
-        for(int i=0;i<26;i++)
+       for(int i=0;i<26;i++)
           if(v[i]>0)
             c++;     
         return(c);
