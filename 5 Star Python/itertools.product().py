fe0846a473ff9e52548fbbc71eb272f0c5ea9337
Alok Khansali <75980074+TheCodeAlpha26@users.noreply.github.com>
2021-11-19 22:28:43 +0530

    Update itertools.product().py

--- 5 Star Python/itertools.product().py
+++ 5 Star Python/itertools.product().py
@@ -1,3 +1,3 @@
 from itertools import product
 a,b = list(map(int,input().split())),list(map(int,input().split()))
-print(*product(a,b))
+print(*product(a,b))                 # * is the unpacker symbol for lists, ** is unpacker symbol for dictionary
