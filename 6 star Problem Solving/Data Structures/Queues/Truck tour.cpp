a5ddd5c313031b29ee4bd7b398a987f7999818b8
Alok Khansali <alokkhansali2002@gmail.com>
2023-01-08 10:57:02 +0530

    Update Truck tour.cpp

--- 6 star Problem Solving/Data Structures/Queues/Truck tour.cpp
+++ 6 star Problem Solving/Data Structures/Queues/Truck tour.cpp
@@ -4,23 +4,24 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n,pt=0,s=0,p=0,d=0;
-    cin >>n;
-    queue<pair<long,long>> ps;     //Using pairs as the elements of teh queue
-    for(int i= 0;i< n;i++){
-           cin>>p>>d;
-           ps.push({p,d});
-       }
-    for(int i=0;i<n;i++)
+    int n, pt = 0, s = 0, p = 0, d = 0;
+    cin >> n;
+    queue<pair<long, long>> ps; // Using pairs as the elements of the queue
+    for (int i = 0; i < n; i++)
     {
-        s=s+ps.front().first-ps.front().second;
-        if(s<0)
+        cin >> p >> d;
+        ps.push({p, d});
+    }
+    for (int i = 0; i < n; i++)
+    {
+        s = s + ps.front().first - ps.front().second;
+        if (s < 0)
         {
-            s=d=0;
-            pt=i+1;
+            s = 0;
+            pt = i + 1;
         }
         ps.pop();
     }
-    cout <<((pt==n)?-1:pt)<<"\n";           //if pt=-1 no solution
+    cout << ((pt == n) ? -1 : pt) << "\n"; // if pt=-1 no solution
     return 0;
 }
